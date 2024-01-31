7c25069a753821c84f8ee68ff488ba856fcc2b90
Xuan-Li@ROG <chen_alphonse_xuanli@outlook.com>
2024-01-31 19:19:35 +0800

    Update inverse_depth_depth_anything.py

--- inverse_depth_depth_anything.py
+++ inverse_depth_depth_anything.py
@@ -1,22 +1,36 @@
-__author__ = 'Xuanli CHEN'
-"""
-Xuanli Chen
-Research Domain: Computer Vision, Machine Learning
-Email: xuanli(dot)chen(at)icloud.com
-LinkedIn: https://be.linkedin.com/in/xuanlichen
-"""
-from transformers import pipeline
-from PIL import Image
-import requests
+from transformers import AutoImageProcessor, AutoModelForDepthEstimation
 import torch
-# --- Set Torch hub
+import numpy as np
+from PIL import Image
+from pathlib import Path
+import requests
 
-# load pipe
-pipe = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
+# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
 
-# load image
-url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-image = Image.open(requests.get(url, stream=True).raw)
+fp_img = Path(r'D:\mono-metric-depth\data\wild_road\frame-002876_1701313567400.jpg')
+image = Image.open(fp_img)
 
-# inference
-depth = pipe(image)["depth"]
+image_processor = AutoImageProcessor.from_pretrained("LiheYoung/depth-anything-small-hf")
+model = AutoModelForDepthEstimation.from_pretrained("LiheYoung/depth-anything-small-hf")
+
+# prepare image for the model
+inputs = image_processor(images=image, return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model(**inputs)
+    predicted_depth = outputs.predicted_depth
+
+# interpolate to original size
+prediction = torch.nn.functional.interpolate(
+    predicted_depth.unsqueeze(1),
+    size=image.size[::-1],
+    mode="bicubic",
+    align_corners=False,
+)
+
+# visualize the prediction
+output = prediction.squeeze().cpu().numpy()
+formatted = (output * 255 / np.max(output)).astype("uint8")
+depth = Image.fromarray(formatted)
+depth.show()
