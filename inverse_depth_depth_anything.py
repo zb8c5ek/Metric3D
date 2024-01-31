b9488f7abd15f05ff35b7f070eeb52e70ae07d9d
Xuan-Li@ROG <chen_alphonse_xuanli@outlook.com>
2024-01-31 19:08:51 +0800

    added data

---
+++ inverse_depth_depth_anything.py
@@ -0,0 +1,22 @@
+__author__ = 'Xuanli CHEN'
+"""
+Xuanli Chen
+Research Domain: Computer Vision, Machine Learning
+Email: xuanli(dot)chen(at)icloud.com
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
+from transformers import pipeline
+from PIL import Image
+import requests
+import torch
+# --- Set Torch hub
+
+# load pipe
+pipe = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
+
+# load image
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+# inference
+depth = pipe(image)["depth"]
