bd5139813006389bb6edd4616cccc07ee18a9948
Xuan-Li@ROG <chen_alphonse_xuanli@outlook.com>
2024-02-04 16:48:43 +0800

    the test is working, need to converge CD on these 2: metric 3D + o3D and COLMAP + ...

--- mono/utils/do_test.py
+++ mono/utils/do_test.py
@@ -13,14 +13,16 @@ import matplotlib.pyplot as plt
 
 from mono.utils.unproj_pcd import reconstruct_pcd, save_point_cloud
 
+
 def to_cuda(data: dict):
     for k, v in data.items():
         if isinstance(v, torch.Tensor):
             data[k] = v.cuda(non_blocking=True)
-        if isinstance(v, list) and len(v)>=1 and isinstance(v[0], torch.Tensor):
+        if isinstance(v, list) and len(v) >= 1 and isinstance(v[0], torch.Tensor):
             for i, l_i in enumerate(v):
                 data[k][i] = l_i.cuda(non_blocking=True)
     return data
+
 
 def align_scale(pred: torch.tensor, target: torch.tensor):
     mask = target > 0
@@ -30,6 +32,7 @@ def align_scale(pred: torch.tensor, target: torch.tensor):
         scale = 1
     pred_scaled = pred * scale
     return pred_scaled, scale
+
 
 def align_scale_shift(pred: torch.tensor, target: torch.tensor):
     mask = target > 0
@@ -45,6 +48,7 @@ def align_scale_shift(pred: torch.tensor, target: torch.tensor):
         shift = 0
     pred = pred * scale + shift
     return pred, scale
+
 
 def align_scale_shift_numpy(pred: np.array, target: np.array):
     mask = target > 0
@@ -62,7 +66,7 @@ def align_scale_shift_numpy(pred: np.array, target: np.array):
     return pred, scale
 
 
-def build_camera_model(H : int, W : int, intrinsics : list) -> np.array:
+def build_camera_model(H: int, W: int, intrinsics: list) -> np.array:
     """
     Encode the camera intrinsic parameters (focal length and principle point) to a 4-channel map. 
     """
@@ -71,11 +75,11 @@ def build_camera_model(H : int, W : int, intrinsics : list) -> np.array:
     # principle point location
     x_row = np.arange(0, W).astype(np.float32)
     x_row_center_norm = (x_row - u0) / W
-    x_center = np.tile(x_row_center_norm, (H, 1)) # [H, W]
+    x_center = np.tile(x_row_center_norm, (H, 1))  # [H, W]
 
-    y_col = np.arange(0, H).astype(np.float32) 
+    y_col = np.arange(0, H).astype(np.float32)
     y_col_center_norm = (y_col - v0) / H
-    y_center = np.tile(y_col_center_norm, (W, 1)).T # [H, W]
+    y_center = np.tile(y_col_center_norm, (W, 1)).T  # [H, W]
 
     # FoV
     fov_x = np.arctan(x_center / (f / W))
@@ -83,6 +87,7 @@ def build_camera_model(H : int, W : int, intrinsics : list) -> np.array:
 
     cam_model = np.stack([x_center, y_center, fov_x, fov_y], axis=2)
     return cam_model
+
 
 def resize_for_input(image, output_shape, intrinsic, canonical_shape, to_canonical_ratio):
     """
@@ -110,51 +115,51 @@ def resize_for_input(image, output_shape, intrinsic, canonical_shape, to_canonic
     image = cv2.resize(image, dsize=(reshape_w, reshape_h), interpolation=cv2.INTER_LINEAR)
     # padding
     image = cv2.copyMakeBorder(
-        image, 
-        pad_h_half, 
-        pad_h - pad_h_half, 
-        pad_w_half, 
-        pad_w - pad_w_half, 
-        cv2.BORDER_CONSTANT, 
+        image,
+        pad_h_half,
+        pad_h - pad_h_half,
+        pad_w_half,
+        pad_w - pad_w_half,
+        cv2.BORDER_CONSTANT,
         value=padding)
-    
+
     # Resize, adjust principle point
     intrinsic[2] = intrinsic[2] * to_scale_ratio
     intrinsic[3] = intrinsic[3] * to_scale_ratio
 
     cam_model = build_camera_model(reshape_h, reshape_w, intrinsic)
     cam_model = cv2.copyMakeBorder(
-        cam_model, 
-        pad_h_half, 
-        pad_h - pad_h_half, 
-        pad_w_half, 
-        pad_w - pad_w_half, 
-        cv2.BORDER_CONSTANT, 
+        cam_model,
+        pad_h_half,
+        pad_h - pad_h_half,
+        pad_w_half,
+        pad_w - pad_w_half,
+        cv2.BORDER_CONSTANT,
         value=-1)
 
-    pad=[pad_h_half, pad_h - pad_h_half, pad_w_half, pad_w - pad_w_half]
-    label_scale_factor=1/to_scale_ratio
+    pad = [pad_h_half, pad_h - pad_h_half, pad_w_half, pad_w - pad_w_half]
+    label_scale_factor = 1 / to_scale_ratio
     return image, cam_model, pad, label_scale_factor
 
 
 def get_prediction(
-    model: torch.nn.Module,
-    input: torch.tensor,
-    cam_model: torch.tensor,
-    pad_info: torch.tensor,
-    scale_info: torch.tensor,
-    gt_depth: torch.tensor,
-    normalize_scale: float,
-    ori_shape: list=[],
+        model: torch.nn.Module,
+        input: torch.tensor,
+        cam_model: torch.tensor,
+        pad_info: torch.tensor,
+        scale_info: torch.tensor,
+        gt_depth: torch.tensor,
+        normalize_scale: float,
+        ori_shape: list = [],
 ):
-
     data = dict(
         input=input,
         cam_model=cam_model,
     )
     pred_depth, confidence, output_dict = model.module.inference(data)
     pred_depth = pred_depth.squeeze()
-    pred_depth = pred_depth[pad_info[0] : pred_depth.shape[0] - pad_info[1], pad_info[2] : pred_depth.shape[1] - pad_info[3]]
+    pred_depth = pred_depth[pad_info[0]: pred_depth.shape[0] - pad_info[1],
+                 pad_info[2]: pred_depth.shape[1] - pad_info[3]]
     if gt_depth is not None:
         resize_shape = gt_depth.shape
     elif ori_shape != []:
@@ -162,7 +167,8 @@ def get_prediction(
     else:
         resize_shape = pred_depth.shape
 
-    pred_depth = torch.nn.functional.interpolate(pred_depth[None, None, :, :], resize_shape, mode='bilinear').squeeze() # to original size
+    pred_depth = torch.nn.functional.interpolate(pred_depth[None, None, :, :], resize_shape,
+                                                 mode='bilinear').squeeze()  # to original size
     pred_depth = pred_depth * normalize_scale / scale_info
     if gt_depth is not None:
         pred_depth_scale, scale = align_scale(pred_depth, gt_depth)
@@ -171,6 +177,7 @@ def get_prediction(
         scale = None
 
     return pred_depth, pred_depth_scale, scale
+
 
 def transform_test_data_scalecano(rgb, intrinsic, data_basic):
     """
@@ -202,7 +209,8 @@ def transform_test_data_scalecano(rgb, intrinsic, data_basic):
     ]
 
     # resize
-    rgb, cam_model, pad, resize_label_scale_ratio = resize_for_input(rgb, forward_size, canonical_intrinsic, [ori_h, ori_w], 1.0)
+    rgb, cam_model, pad, resize_label_scale_ratio = resize_for_input(rgb, forward_size, canonical_intrinsic,
+                                                                     [ori_h, ori_w], 1.0)
 
     # label scale factor
     label_scale_factor = cano_label_scale_ratio * resize_label_scale_ratio
@@ -210,24 +218,25 @@ def transform_test_data_scalecano(rgb, intrinsic, data_basic):
     rgb = torch.from_numpy(rgb.transpose((2, 0, 1))).float()
     rgb = torch.div((rgb - mean), std)
     rgb = rgb[None, :, :, :].cuda()
-    
+
     cam_model = torch.from_numpy(cam_model.transpose((2, 0, 1))).float()
     cam_model = cam_model[None, :, :, :].cuda()
     cam_model_stacks = [
-        torch.nn.functional.interpolate(cam_model, size=(cam_model.shape[2]//i, cam_model.shape[3]//i), mode='bilinear', align_corners=False)
+        torch.nn.functional.interpolate(cam_model, size=(cam_model.shape[2] // i, cam_model.shape[3] // i),
+                                        mode='bilinear', align_corners=False)
         for i in [2, 4, 8, 16, 32]
     ]
     return rgb, cam_model_stacks, pad, label_scale_factor
 
-def do_scalecano_test_with_custom_data(
-    model: torch.nn.Module,
-    cfg: dict,
-    test_data: list,
-    logger: logging.RootLogger,
-    is_distributed: bool = True,
-    local_rank: int = 0,
-):
 
+def do_scalecano_test_with_custom_data(
+        model: torch.nn.Module,
+        cfg: dict,
+        test_data: list,
+        logger: logging.RootLogger,
+        is_distributed: bool = True,
+        local_rank: int = 0,
+):
     show_dir = cfg.show_dir
     save_interval = 1
     save_imgs_dir = show_dir + '/vis'
@@ -239,9 +248,9 @@ def do_scalecano_test_with_custom_data(
     dam = MetricAverageMeter(['abs_rel', 'rmse', 'silog', 'delta1', 'delta2', 'delta3'])
     dam_median = MetricAverageMeter(['abs_rel', 'rmse', 'silog', 'delta1', 'delta2', 'delta3'])
     dam_global = MetricAverageMeter(['abs_rel', 'rmse', 'silog', 'delta1', 'delta2', 'delta3'])
-    
+
     for i, an in tqdm(enumerate(test_data)):
-        #rgb_origin = cv2.imread(an['rgb'])[:, :, ::-1].copy()
+        # rgb_origin = cv2.imread(an['rgb'])[:, :, ::-1].copy()
         rgb_origin = cv2.imread(an['rgb'])
         if an['depth'] is not None:
             gt_depth = cv2.imread(an['depth'], -1)
@@ -253,33 +262,35 @@ def do_scalecano_test_with_custom_data(
             gt_depth_flag = False
         intrinsic = an['intrinsic']
         if intrinsic is None:
-            intrinsic = [1000.0, 1000.0, rgb_origin.shape[1]/2, rgb_origin.shape[0]/2]
-        rgb_input, cam_models_stacks, pad, label_scale_factor = transform_test_data_scalecano(rgb_origin, intrinsic, cfg.data_basic)
+            intrinsic = [1000.0, 1000.0, rgb_origin.shape[1] / 2, rgb_origin.shape[0] / 2]
+        rgb_input, cam_models_stacks, pad, label_scale_factor = transform_test_data_scalecano(rgb_origin, intrinsic,
+                                                                                              cfg.data_basic)
 
         pred_depth, pred_depth_scale, scale = get_prediction(
-            model = model,
-            input = rgb_input,
-            cam_model = cam_models_stacks,
-            pad_info = pad,
-            scale_info = label_scale_factor,
-            gt_depth = None,
-            normalize_scale = normalize_scale,
+            model=model,
+            input=rgb_input,
+            cam_model=cam_models_stacks,
+            pad_info=pad,
+            scale_info=label_scale_factor,
+            gt_depth=None,
+            normalize_scale=normalize_scale,
             ori_shape=[rgb_origin.shape[0], rgb_origin.shape[1]],
         )
 
         if gt_depth_flag:
-
-            pred_depth = torch.nn.functional.interpolate(pred_depth[None, None, :, :], (gt_depth.shape[0], gt_depth.shape[1]), mode='bilinear').squeeze() # to original size
+            pred_depth = torch.nn.functional.interpolate(pred_depth[None, None, :, :],
+                                                         (gt_depth.shape[0], gt_depth.shape[1]),
+                                                         mode='bilinear').squeeze()  # to original size
             gt_depth = torch.from_numpy(gt_depth).cuda()
 
             pred_depth_median = pred_depth * gt_depth[gt_depth != 0].median() / pred_depth[gt_depth != 0].median()
             pred_global, _ = align_scale_shift(pred_depth, gt_depth)
-            
+
             mask = (gt_depth > 1e-8)
             dam.update_metrics_gpu(pred_depth, gt_depth, mask, is_distributed)
             dam_median.update_metrics_gpu(pred_depth_median, gt_depth, mask, is_distributed)
             dam_global.update_metrics_gpu(pred_global, gt_depth, mask, is_distributed)
-        
+
         if i % save_interval == 0:
             os.makedirs(osp.join(save_imgs_dir, an['folder']), exist_ok=True)
             rgb_torch = torch.from_numpy(rgb_origin).to(pred_depth.device).permute(2, 0, 1)
@@ -300,20 +311,24 @@ def do_scalecano_test_with_custom_data(
             pred_depth = pred_depth.detach().cpu().numpy()
             pcd = reconstruct_pcd(pred_depth, intrinsic[0], intrinsic[1], intrinsic[2], intrinsic[3])
             os.makedirs(osp.join(save_pcd_dir, an['folder']), exist_ok=True)
-            
+
             if an['intrinsic'] != None:
-                save_point_cloud(pcd.reshape((-1, 3)), rgb_origin.reshape(-1, 3), osp.join(save_pcd_dir, an['folder'], an['filename'][:-4]+'.ply'))
+                save_point_cloud(pcd.reshape((-1, 3)), rgb_origin.reshape(-1, 3),
+                                 osp.join(save_pcd_dir, an['folder'], an['filename'][:-4] + '.ply'))
             else:
                 # for r in [0.9, 1.0, 1.1]:
                 #     for f in [600, 800, 1000, 1250, 1500]:
                 for r in [1.0]:
                     for f in [1000]:
-                        #f_scale = f
-                        f_scale = f * (rgb_origin.shape[0] + rgb_origin.shape[1]) / (cfg.data_basic.canonical_space['img_size'][0] + cfg.data_basic.canonical_space['img_size'][1])
+                        # f_scale = f
+                        f_scale = f * (rgb_origin.shape[0] + rgb_origin.shape[1]) / (
+                                    cfg.data_basic.canonical_space['img_size'][0] +
+                                    cfg.data_basic.canonical_space['img_size'][1])
                         pcd = reconstruct_pcd(pred_depth, f_scale * r, f_scale * (2 - r), intrinsic[2], intrinsic[3])
-                        fstr = '_fx_' + str(int(f_scale * r)) + '_fy_' + str(int(f_scale * (2-r)))
-                        save_point_cloud(pcd.reshape((-1, 3)), rgb_origin.reshape(-1, 3), osp.join(save_pcd_dir, an['folder'], an['filename'][:-4]+fstr+'.ply'))
-    
+                        fstr = '_fx_' + str(int(f_scale * r)) + '_fy_' + str(int(f_scale * (2 - r)))
+                        save_point_cloud(pcd.reshape((-1, 3)), rgb_origin.reshape(-1, 3),
+                                         osp.join(save_pcd_dir, an['folder'], an['filename'][:-4] + fstr + '.ply'))
+
     if gt_depth_flag:
         eval_error = dam.get_metrics()
         print('w/o match :', eval_error)
