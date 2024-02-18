a8c6ed69a21c00c7265103e4712999369ff651d4
Xuan-Li@ROG <chen_alphonse_xuanli@outlook.com>
2024-02-18 14:26:58 +0800

    added camera files

---
+++ MonoMetricOdometry/FrontTwoAlignEssential.py
@@ -0,0 +1,12 @@
+__author__ = 'Xuanli CHEN'
+"""
+Xuanli Chen
+Research Domain: Computer Vision, Machine Learning
+Email: xuanli(dot)chen(at)icloud.com
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
+"""
+the sample data is from: "E:\psd_data\data_calib_20231129\work_dir_epsilon_0.1\length_0060_0070_num_16\segment_0000"
+in which is the 30 degree data. then the other camera data is at:
+
+"""
